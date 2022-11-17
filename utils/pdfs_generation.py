97935b8a6d23ee8add95ea4dc9844a9b886d3159
Jose Javier Rosales <rosalesruizjosejavier@gmail.com>
2022-11-17 22:24:37 +0000

    Update pdfs_generation.py functions
    This commit applies the following changes:
     - Add new function to plot a given distribution
     - Add new function to fit probability density distribution given a set
     of data.
     - Re-estructure function to find the best fit PDF.

--- utils/pdfs_generation.py
+++ utils/pdfs_generation.py
@@ -30,68 +30,93 @@ cwd_images  = os.path.join(cwd, 'images')
 cwd_data    = os.path.join(cwd, 'data')
 
 #%%
-# Create models from data
-def best_fit_distribution(data, bins=200, exclude=[]):
-    """Model data by finding best fit distribution to data"""
+def fit_distribution(stdist, data, bins):
+    
     # Get histogram of original data
     y, x = np.histogram(data, bins=bins, density=True)
     x = (x + np.roll(x, -1))[:-1] / 2.0
+    
+    # Best holders
+    dist = {'dist': st.norm,
+            'params': (0.0, 1.0),
+            'sse': np.inf}
+    
+    # Try to fit the distribution
+    try:
+        # Ignore warnings from data that can"t be fit
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore")
+
+            # fit dist to data
+            params = stdist.fit(data)
+
+            # Separate parts of parameters
+            arg = params[:-2]
+            loc = params[-2]
+            scale = params[-1]
+
+            # Calculate fitted PDF and error with fit in distribution
+            pdf = stdist.pdf(x, loc=loc, scale=scale, *arg)
+            sse = np.sum(np.power(y - pdf, 2.0))
+
+            # identify if this distribution is better
+            dist['dist']    = stdist
+            dist['params']  = params
+            dist['sse']     = sse
+            
+    except Exception:
+        pass
+    
+    return dist
+
+#%%
+# Create models from data
+def find_best_distribution(data, bins=200, st_excluded=[]):
+    """Model data by finding best fit distribution to data"""
     
     # Get Continuous distributions table from SciPy website.
     url = 'https://docs.scipy.org/doc/scipy/reference/stats.html'
     dist_table = pd.read_html(requests.get(url).content)[1]
     
     # Evaluate list of objects in str format to convert it into a Python list
-    dist_list = eval('[' + (', ').join('st.' + dist_table[0]) + ']')
+    stdist_list = eval('[' + (', ').join('st.' + dist_table[0]) + ']')
     
     # Best holders
-    best = {'dist': st.norm,
+    best_dist = {'dist': st.norm,
             'params': (0.0, 1.0),
             'sse': np.inf}
     
     dists_checked = []
 
     # Estimate distribution parameters from data
-    for i, dist in enumerate(dist_list):
+    for i, stdist in enumerate(stdist_list):
         
         
         print('Progress %5.1f%% (%3d/%3d) \tDistribution: %16s \tBest: %s' %
-              ((i+1)/len(dist_list)*100, i, len(dist_list)-1, dist.name, 
-               best['dist'].name))
+              ((i+1)/len(stdist_list)*100, i, len(stdist_list)-1, stdist.name, 
+               best_dist['dist'].name))
         
-        if dist.name in exclude: continue
+        if stdist.name in st_excluded: continue
         
-        # Try to fit the distribution
-        try:
-            # Ignore warnings from data that can"t be fit
-            with warnings.catch_warnings():
-                warnings.filterwarnings("ignore")
-
-                # fit dist to data
-                params = dist.fit(data)
-
-                # Separate parts of parameters
-                arg = params[:-2]
-                loc = params[-2]
-                scale = params[-1]
-
-                # Calculate fitted PDF and error with fit in distribution
-                pdf = dist.pdf(x, loc=loc, scale=scale, *arg)
-                sse = np.sum(np.power(y - pdf, 2.0))
-
-                # identify if this distribution is better
-                if best['sse'] > sse > 0:
-                    best['dist']= dist
-                    best['params'] = params
-                    best['sse'] = sse
-
-        except Exception:
-            pass
+        dist = fit_distribution(stdist, data, bins)
         
-        dists_checked.append(dist.name)
+        if best_dist['sse'] > dist['sse'] > 0: best_dist = dist
         
-    print("\nMinimum error: %1.3e" % best['sse'])
-    return (best['dist'].name, best['params'])
+        dists_checked.append(stdist.name)
+        
+    # Get information of the parameters
+    param_names = (dist['dist'].shapes + ", loc, scale").split(", ") \
+                    if dist['dist'].shapes else ["loc", "scale"]
+        
+    
+    params = ""
+    for p, v in zip(param_names, best_dist['params']): 
+        params = params + "\n - "+ p + "=" + str(v)
+        
+    print("\nBest dist.: %s\nSSE: %1.3e\nParameters:%s" % \
+          (best_dist['dist'].name, best_dist['sse'], params))
+    
+    return best_dist
 
 #%%
 def make_pdf(dist, params, size=10000):
@@ -115,6 +140,41 @@ def make_pdf(dist, params, size=10000):
 
     return pdf
 
+#%%
+def plot_dist(data, dist, xlabel, bins, figurename):
+    
+    # Make Probability Density Function with distribution parameters 
+    pdf = make_pdf(dist['dist'], dist['params'])
+    
+    # Get information of the parameters
+    param_names = (dist['dist'].shapes + ", loc, scale").split(", ") \
+        if dist['dist'].shapes else ["loc", "scale"]
+            
+    # Create string with all parameters to print into the plot's title       
+    title = (dist['dist'].name).capitalize() + '\n' + \
+                "\quad ".join(["{}={:0.5f}".format(k,v) \
+                for k,v in zip(param_names, dist['params'])])
+
+    # Display plot
+    plt.figure(figsize=(7,5))
+
+    ax = pdf.plot(lw=1.5, color = "orange", label="PDF")
+    data.plot(kind="hist", bins=bins, ax=ax, density=True, 
+              label="Data", color="dimgrey")
+    
+    plt.title("{}".format(title), fontsize=12)
+    plt.xlabel(xlabel)
+    plt.ylabel(r"Probability density")
+    plt.grid(True, linestyle="dashed", alpha=0.5)
+    plt.legend(loc="best", fontsize=10)
+    plt.savefig(os.path.join(cwd_images, 
+                             'probability-density', 
+                             figurename + ".pdf"),
+                bbox_inches="tight")
+    plt.show()
+    
+    return
+
 
 #%%
 def get_probabilities(dist, arg, loc, scale, size=10000):
@@ -135,15 +195,8 @@ def get_probabilities(dist, arg, loc, scale, size=10000):
 #%%
 if __name__ == "__main__":
      
-    
-    
-    n_bins = 50
-    column = "miss_distance"
-    
     # Import Kelvins data
-    df = pd.read_csv(os.path.join(cwd_data, 
-                                  'esa-challenge',
-                                  'train_data.csv'), 
+    df = pd.read_csv(os.path.join(cwd_data, 'esa-challenge', 'train_data.csv'), 
                      sep=',', header=0, index_col=None, skipinitialspace=False)
     
     # Sort values of dataframe by event_id and time_to_tca and re-index
@@ -153,44 +206,20 @@ if __name__ == "__main__":
     # Get only last CDM data from every event_id
     df = df.drop_duplicates('event_id', keep='last')
     
+    # Set column name to study and remove outliers.
+    column = "miss_distance"
     data = gn.remove_outliers(df[column], 1.5)
     
+    # Find distribution that best fits the data
+    best_dist = find_best_distribution(data, 200, ['studentized_range'])
+    
+    # Fit distribution to the data
+    dist = fit_distribution(st.expon, data, 200)
+    
+    # Print plot including histogram and distribution fitted
+    plot_dist(data, dist, column, 
+              50, column + '-' + dist['dist'].name)
+    
 
-    # Find best fit distribution
-    dist_name, dist_params = best_fit_distribution(data, 200, ['studentized_range'])
-    dist = getattr(st, dist_name)
-    
-    # Get information of the parameters
-    param_names = (dist.shapes + ", loc, scale").split(", ") \
-        if dist.shapes else ["loc", "scale"]
-    param_str = "\quad ".join(["{}={:0.5f}".format(k,v) \
-                               for k,v in zip(param_names, dist_params)])
-
-    print("Best distribution:", dist.name)
-    for k,v in zip(param_names, dist_params): print(k,"=", v)
-        
-    # Make PDF with best params 
-    pdf = make_pdf(dist, dist_params)
-    
-    figurename = column + "_" + dist_name + ".pdf"
-    
-    # Display  plot
-    plt.figure(figsize=(7,5))
-
-    ax = pdf.plot(lw=1.5, color = "orange", 
-                  label=dist_name.capitalize() +" PDF")
-    data.plot(kind="hist", bins=n_bins, ax=ax, 
-              density=True, label="Kelvins data", color="dimgrey")
-    
-    plt.title("{}".format(param_str), fontsize=12)
-    plt.xlabel(column)
-    plt.ylabel(r"Probability density")
-    plt.grid(True, linestyle="dashed", alpha=0.5)
-    plt.legend(loc="best")
-    plt.savefig(os.path.join(cwd_images, 
-                             'probability-density', 
-                             figurename),
-                bbox_inches="tight")
-    plt.show()
     
     
