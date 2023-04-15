9d46434578fe3d8926b1aee5018eefa884e72bbe
Jose Javier Rosales <rosalesruizjosejavier@gmail.com>
2023-04-15 20:51:33 +0100

    Add msecv function to bw_selection library

--- library/irplib/bw_selection.py
+++ library/irplib/bw_selection.py
@@ -74,3 +74,104 @@ def plot_kde(data:np.ndarray, bandwidths:np.ndarray, **kwargs) -> None:
     plt.show()
     
     return None
+
+#%%
+def msecv(data: np.ndarray, bins:dict, conv_accuracy:float = 1e-5, 
+                    n_batches_max:int = 10, print_log:bool = True) -> dict:
+    """Computes optimal bandwidth minimizing MSE actual vs estimated density through cross-validation.
+
+    Args:
+        data (np.ndarray): Array containing all input data.
+        bins (dict): _description_
+        conv_accuracy (float, optional): Convergence accuracy. Defaults to 1e-5.
+        n_batches_max (int, optional): Maximum number of batches for the cross-validation. Defaults to 10.
+        print_log (bool, optional): Print computational log. Defaults to True.
+
+    Returns:
+        dict: Dictionary with the bandwidth value that minimizes the MSE and the estimated MSE.
+    """
+    
+    # Exclude NaN and non-finite numbers from data
+    data = data[np.isfinite(data)]
+
+    # Create an array with the number of batches to process per iteration 
+    batches_list = np.arange(start=2, stop=n_batches_max + 1, step=1, dtype=np.int32)
+    
+    # Initialize arry to store best bandwidth per group of batches
+    best_bandwidths = np.zeros(len(batches_list))
+
+    for i, batches in enumerate(batches_list):
+        
+        # Create an array with random values between 0 and the total number of batches
+        # of the size of the entire array of data (following a uniform distribution).
+        batches_categories = np.random.randint(low=0, high=batches, size=len(data))
+
+        # Initialize array to store actual probabilty densities (apds) from every batch
+        apds = np.zeros((bins['n'], batches))
+
+        # Iterate over all batches categories (0 to number of batches)
+        for c in range(batches):
+            
+            # Get sample of data corresponding to a specific batch category "c"
+            sample = data[batches_categories==c]
+            
+            # Get actual probability density from the sample using number of bins obtained
+            # from the entire array of data and store it in the apds array.
+            apds[:,c], bin_edges = np.histogram(sample, bins = bins['n'], density=True)
+
+        # Get array of bin centers to which the actual probability density are associated
+        bin_centers = bin_edges[:-1] + bins['width']/2
+
+        # Compute average of all actual probability densities from all batches
+        avg_apd = np.mean(apds, axis=1, dtype=np.float64)
+
+        # Initialize bandwidth array to evaluate estimated probability density from kernel
+        bandwidths, step = np.linspace(bins['width']/100, bins['width'], 100, retstep=True)
+
+        # Initialize best_bw
+        best_bw = 0.0
+        while True:
+
+            # Initialize mean squared errors array associated to every bandwidth
+            mse = np.zeros(len(bandwidths))
+
+            # Iterate over all the bandwidths to compute the MSE from the actual vs estimated 
+            # probability densities
+            for b, bandwidth in enumerate(bandwidths):
+
+                # Get estimated probability distribution using the bandwidth "b"
+                epd = bws.kde(data, bin_centers, bandwidth=bandwidth)
+                
+                # Compute MSE from actual vs estimated probability densities
+                mse[b] = ((epd - avg_apd)**2).mean()
+                
+            # Get bandwidth that minimizes MSE and check accuracy vs best_bw
+            bw = bandwidths[np.argmin(mse)]
+
+            # Check if convergence accuracy is achieved to stop iterations
+            if abs(1 - best_bw/bw) <= conv_accuracy: break
+            
+            # Update best_bw and bandwidths array to increase final bandwidth accuracy
+            best_bw = bw
+            bandwidths, step = np.linspace(bw-step, bw+step, 100, retstep = True)
+            
+        # Add best bandwidth from this group of batches to final array
+        best_bandwidths[i] = best_bw 
+
+        if print_log: print(f'Batches = {batches:2} ({len(data)//batches} d.p. per batch)  '
+                            f'Best bandwidth = {best_bw:.5f}  MSE(apd, epd) = {mse.min():.4e}')
+
+    # Round-up best bandwidth from all groups of batches using one order of magnitude less
+    scale = 10**main.order_of_magnitude(best_bandwidths.mean())
+    best_bw = (math.ceil(best_bandwidths.mean()/scale)*scale)
+    
+    
+    # Compute final estimated probability density using the best bandwidth and compare it
+    # with actual probability density using MSE
+    epd = bws.kde(data, bin_centers, kernel='gaussian', bandwidth=best_bw)
+    apd, bin_edges = np.histogram(data, bins = bins['n'], density=True)
+    estimated_mse = ((epd - apd)**2).mean()
+    
+    if print_log: print(f'\nFinal Optimal bandwidth = {best_bw}\t MSE(apd, epd) = {estimated_mse}')
+    
+    return {'bw': best_bw, 'estimated_mse':estimated_mse}
