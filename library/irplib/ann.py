2d397dfb237579266a0b4ac4cf7541175005bb3a
Jose Javier Rosales <rosalesruizjosejavier@gmail.com>
2023-07-04 21:58:28 +0100

    Add event_ts_sets function to ann library
    This commit applies the following changes:
     - Add event_ts_sets functions which gets an list of tuples in the
     format ([feature1_t1, feature1_t2, time_21], [feature1_t3, time23])
     where the first element of the tuple is the input for a vanilla ANN and
     the second element is the output to train the model.
     - Adjust formatting of code to stick to 80 chars width.

--- library/irplib/ann.py
+++ library/irplib/ann.py
@@ -11,36 +11,38 @@ class CollisionRisk(nn.Module):
         # Inherit attributes from nn.Module class
         super().__init__()
         
-        #############################################################################
+        ########################################################################
         # Instanciate functions to use on the forward operation:
         
-        # self.embeds: Creates a list of pre-configured Embedding operations (it is 
-        # configured by passing the number of categories ni and the length of the 
-        # embedding nf)
+        # self.embeds: Creates a list of pre-configured Embedding operations (it 
+        # is configured by passing the number of categories ni and the length of
+        #  the embedding nf)
         self.embeds = nn.ModuleList([nn.Embedding(ni, nf) for ni,nf in emb_szs])
         
         # self.emb_drop: Cancels a proportion p of the embeddings.
         self.emb_drop = nn.Dropout(p)
         
-        # self.bn_cont = Normalizes continuous features. This function is configured
-        # by passing the number of continuous features to normalize.
+        # self.bn_cont = Normalizes continuous features. This function is 
+        # configured by passing the number of continuous features to normalize.
         self.bn_cont = nn.BatchNorm1d(n_cont)
         
-        #############################################################################
-        # Count total number of embeddings (Total number of vector components for
-        # every feature)
+        ########################################################################
+        # Count total number of embeddings (Total number of vector components 
+        # for every feature)
         n_emb = sum((nf for ni,nf in emb_szs))
         
-        # Compute total number of inputs to pass to the initial layer (data point = 
-        # Nb. of embeddings + Nb. of continuous variables)
+        # Compute total number of inputs to pass to the initial layer (data 
+        # point = Nb. of embeddings + Nb. of continuous variables)
         n_in = n_emb + n_cont
         
         # Run through every layer to set up the operations to perform per layer.
         # (i.e. layers=[100, 50, 200])
         layerlist = []
         for l, n_neurons in enumerate(layers):
-            # On layer l, which contains n_neurons, perform the following operations:
-            # 1. Apply Linear neural network model regression (fully connected network -> z = Sum(wi*xi+bi))
+            # On layer l, which contains n_neurons, perform the following 
+            # operations:
+            # 1. Apply Linear neural network model regression (fully connected 
+            # network -> z = Sum(wi*xi+bi))
             layerlist.append(nn.Linear(n_in,n_neurons))
             
             # 2. Apply ReLU activation function (al(z))
@@ -49,7 +51,8 @@ class CollisionRisk(nn.Module):
             # 3. Normalize data using the n_neurons
             layerlist.append(nn.BatchNorm1d(n_neurons))
             
-            # 4. Cancel out a random proportion p of the neurons to avoid overfitting
+            # 4. Cancel out a random proportion p of the neurons to avoid 
+            # overfitting
             layerlist.append(nn.Dropout(p))
             
             # 5. Set new number of input features n_in for the next layer l+1.
@@ -82,7 +85,45 @@ class CollisionRisk(nn.Module):
         # Concatenate embeddings with continuous variables into one torch
         x = torch.cat([x, x_cont], 1)
         
-        # Process all data points with the layers functions (sequential of operations)
+        # Process all data points with the layers functions (sequential of 
+        # operations)
         x = self.layers(x)
         
         return x
+
+#%%
+#%%
+def event_ts_sets(feature_seq:np.ndarray, time_seq:np.ndarray) -> list:  
+    """Get all possible Time-Series subsets (sequence->target) from a complete 
+    Time-Series set associated to an event.
+
+    Args:
+        full_sequence (np.ndarray): Array containing the full sequence 
+        of data for a given event.
+        window_size (int): Window size of events.
+        events_to_forecast (int): Number of events to forecast. Defaults to 1.
+
+    Returns:
+        list: List of tuples with sequences and labels.
+    """
+
+    # Get number of TS sets to extract from the full sequence. 
+    n = len(feature_seq) - 2
+
+    # Initialize Time-Series sets list containing tuples with sequence-target
+    # for a given event.
+    ts_sets = []
+
+    # Create the list of Time-Series sets using a loop.
+    for i in range(n):
+
+        # Get sequence and target value for element i
+        seq_i  = np.append(feature_seq[i:i+2],
+                           np.abs(time_seq[i+1]-time_seq[i]))
+        target_i  = np.append(feature_seq[i+2],
+                              np.abs(time_seq[i+2]-time_seq[i+1]))
+
+        # Add tuple to the output list
+        ts_sets.append((seq_i, target_i))
+
+    return ts_sets
