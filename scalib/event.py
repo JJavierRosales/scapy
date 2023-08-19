Fix data normalization in DatasetEventDataset
This commit applies the following changes:
 - Improve ConjunctionEventsDataset module to keep dataframe in cache.
 - Remove library in utils.
 - Fix data normalization in DatasetEventDataset.

scalib/event.py
@@ -16,6 +16,7 @@ from .cdm import ConjunctionDataMessage as CDM
 
 mpl.rcParams['axes.unicode_minus'] = False
 
+
 #%% CLASS: EventsPlotting
 class EventsPlotting():
 
@@ -223,9 +224,10 @@ class ConjunctionEvent(EventsPlotting):
             for cdm in self._cdms:
                 cdm_dataframes.append(cdm.to_dataframe())
             self._dataframe = pd.concat(cdm_dataframes, ignore_index=True)
+            
         return self._dataframe
 
-    def plot_feature(self, feature:str, figsize:tuple = (5, 3), 
+    def plot_feature(self, feature:str, figsize:tuple = (6, 3), 
                      ax:plt.Axes = None, return_ax:bool = False, 
                      apply_func = (lambda x: x), filepath:str = None, 
                      legend:bool = False, xlim:tuple = (-0.5,7.5), 
@@ -308,7 +310,7 @@ class ConjunctionEvent(EventsPlotting):
         ax.set_xticks(np.arange(8))
 
         # Plot legend
-        if legend: ax.legend(loc='best')
+        if legend: ax.legend(loc='best', fontsize = 8)
 
         # Save plot if filepath is provided.
         if filepath is not None:
@@ -401,14 +403,14 @@ class ConjunctionEventsDataset(EventsPlotting):
                 event_cdm_filepaths = []
                 for event_prefix in event_prefixes:
                     # Get list of CDM files whose names match the event_prefix.
-                    cdm_filepaths.append(list(filter(lambda f: 
+                    event_cdm_filepaths.append(list(filter(lambda f: 
                                                      f.startswith(event_prefix), 
                                                      filepaths)))
                 # Append event objects from the filepaths.
                 self._events = [ConjunctionEvent(cdm_filepaths=f) \
-                                for f in cdm_filepaths]
+                                for f in event_cdm_filepaths]
                 print('Loaded {} CDMs grouped into {} events' \
-                      .format(len(file_names), len(self._events)))
+                      .format(len(filepaths), len(self._events)))
         else:
             self._events = events
 
@@ -543,7 +545,9 @@ class ConjunctionEventsDataset(EventsPlotting):
         pb_events.refresh(i = e+1, 
             description='Imported as DataFrame.')
         
-        return pd.concat(event_dataframes, ignore_index=True)
+        self._dataframe = pd.concat(event_dataframes, ignore_index=True)
+        
+        return self._dataframe
 
     def dates(self) -> None:
         """Print summary on datetime information about CDM generation and TCA 

scalib/nn.py
@@ -13,10 +13,9 @@ from matplotlib import pyplot as plt
 import warnings
 
 from . import utils
-from .cdm import ConjunctionDataMessage as CDM
 from .event import ConjunctionEvent as CE
 from .event import ConjunctionEventsDataset as CED
-
+from .cdm import ConjunctionDataMessage as CDM
 
 #%% CLASS: LSTMLayer
 class LSTMLayer(nn.Module):
@@ -329,14 +328,22 @@ class DatasetEventDataset(Dataset):
         # is introduced to remove value errors caused by division by zero.
         epsilon = 1e-8
         for i, cdm in enumerate(event):
+
+            # Initialize list to store normalized values for every feature in a
+            # given CDM.
+            norm_values = []
+
             for j, feature in enumerate(self._features):
                 # Get mean and standard deviation per feature.
                 feature_mean = self._features_stats['mean'][j]
                 feature_stddev = self._features_stats['stddev'][j]
                 
-                # Add normalzied feature to the tensor.
-                x[i] = torch.tensor([(cdm[feature]-feature_mean)/\
-                                     (feature_stddev+epsilon)])
+                # Append normalized values from feature to the list.
+                norm_values += [(cdm[feature] - feature_mean)/
+                                (feature_stddev + epsilon)]
+                
+            # Add normalized values from all features to the output tensor.
+            x[i] = torch.tensor(norm_values)
 
         return x, torch.tensor(len(event))
     
@@ -820,13 +827,13 @@ class ConjunctionEventForecaster(nn.Module):
         events = []
 
         # Iterate over all sequences
-        pb_samples = utils.ProgressBar(iterations = range(num_samples),  
-            description='Forecasting event evolution ...')
+        # pb_samples = utils.ProgressBar(iterations = range(num_samples),  
+        #     description='Forecasting event evolution ...')
 
-        for i in pb_samples.iterations:
+        for i in range(num_samples):
 
             # Update progress bar.
-            pb_samples.refresh(i = i+1)
+            # pb_samples.refresh(i = i+1)
 
             # Create a deep copy of the input event to avoid modifying the 
             # original object
@@ -842,7 +849,14 @@ class ConjunctionEventForecaster(nn.Module):
 
                 # Predict new CDM from i_event and append object to i_event.
                 cdm = self.predict(i_event)
+
                 i_event.add(cdm)
+
+                # Check predicted CDM time to TCA is lower than previous CDM.
+                if cdm['__TCA'] >= i_event[-2]['__TCA']: 
+                    i_event = i_event[:-1]
+                    continue
+
 
                 # Stop loop if one of the conditions is met.
                 if (cdm['__CREATION_DATE'] > cdm['__TCA']) or \
@@ -853,7 +867,7 @@ class ConjunctionEventForecaster(nn.Module):
             events.append(i_event)
 
         # Update progress bar.
-        pb_samples.refresh(i = i+1, description = 'Event forecasted.')
+        # pb_samples.refresh(i = i+1, description = 'Event forecasted.')
 
         return events[0] if num_samples==1 \
             else CED(events = events)